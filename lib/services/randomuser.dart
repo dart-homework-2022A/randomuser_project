f378a148cf06a2acfcb99bf5c00c2800959ddc6f
ZARIFMAC <uzbzarif@gmail.com>
2022-12-06 14:20:44 +0500

    get data from api

--- lib/services/randomuser.dart
+++ lib/services/randomuser.dart
@@ -1,1 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 final String baseUrl = "https://randomuser.me/api/";
+
+Future getData() async {
+  String firstName = 'NoName';
+  // Define the URL
+  Uri url = Uri.parse(baseUrl);
+  // Make the request
+  http.Response response = await http.get(url);
+
+  // Check the status code
+
+  // If the call to the server was successful, parse the JSON
+  Map data = json.decode(response.body);
+  // print(data);
+
+  // // Get user name from JSON
+  if (response.statusCode == 200) {
+    firstName = data['results'][0]['name']['first'];
+  }
+  return firstName;
+}
