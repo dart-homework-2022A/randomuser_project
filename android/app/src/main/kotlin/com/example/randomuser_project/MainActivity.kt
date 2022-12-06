6e6cbd749faffa1993b80c5ac71675205a8bcd69
ZARIFMAC <uzbzarif@gmail.com>
2022-12-06 13:42:33 +0500

    init

---
+++ android/app/src/main/kotlin/com/example/randomuser_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.randomuser_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Title'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Text('Title'),
+        ),
+      ),
+    ),
+  );
+}
