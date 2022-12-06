6132fffc593ae523e9550506dd3a722837c90d14
ZARIFMAC <uzbzarif@gmail.com>
2022-12-06 14:33:10 +0500

    user widget

---
+++ lib/widgets/UserWidget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:randomuser_project/models/User.dart';
+
+class UserWidget extends StatelessWidget {
+  final User user;
+  UserWidget({Key? key, required this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      children: [
+        const Text('User Widget'),
+        Text(user.firstName),
+        Text(user.lastName),
+        Text(user.email),
+      ],
+      mainAxisAlignment: MainAxisAlignment.center,
+    ));
+  }
+}
