8971d397225f2198b63bd323da982d3eff305103
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-12-08 15:28:14 -0800

    the end

--- lib/widgets/UserWidget.dart
+++ lib/widgets/UserWidget.dart
@@ -11,7 +11,7 @@ class UserWidget extends StatelessWidget {
         child: Column(
       children: [
         const Text('User Widget'),
-        Text(user.imageUrl),
+    Image.network(user.imageUrl),
         Text(user.firstName),
         Text(user.lastName),
         Text(user.email),
